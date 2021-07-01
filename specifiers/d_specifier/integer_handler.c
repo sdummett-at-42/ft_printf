d8113248997c955ec066f2531013ba78f395649e
Stone <stone.dummett@gmail.com>
2021-07-01 02:16:40 +0200

    Added struct for flag attributes, added integer_parser (changed the algo), integer_handler now handles positive int with each flag

--- specifiers/d_specifier/integer_handler.c
+++ specifiers/d_specifier/integer_handler.c
@@ -6,205 +6,175 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 02:24:06 by sdummett          #+#    #+#             */
-/*   Updated: 2021/06/30 22:34:49 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/07/01 02:14:46 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../ft_printf.h"
 
-/*
-void	ft_putnbr(long long int nb)
+
+static t_flag_attribs *struct_initializer(void)
 {
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		nb *= -1;
-	}
-	if (nb >= 10)
-	{
-		ft_putnbr(nb / 10);
-		ft_putchar(nb % 10 + 48);
-	}
-	else
-		ft_putchar(nb + 48);	
+	t_flag_attribs *new;
+
+	new = (t_flag_attribs *)malloc(sizeof(t_flag_attribs) * 1);
+	new->star = 0;
+	new->precision = 0;
+	new->width = 0;
+	new->zero = 0;
+	return (new);
 }
-*/
 
-
-static void	ft_putstr_with_width(char *fill, char *str, int width, int side)
+static t_flag_attribs *integer_parser(char **format, va_list var)
 {
-	int len;
+	int i;
+	int neg;
+	t_flag_attribs *spec_infos;
 
-	len = ft_strlen(str);
-	len = ft_strlen(str);
-	if (width != -1)
-		width = width - len;
-	if (width > 0)
+	spec_infos = struct_initializer();
+	i = 0;
+	neg = 1;
+	if (((*format)[i] >= '0' && (*format)[i] <= '9') || (*format)[i] == '-' || \
+			(*format)[i] == '*')
 	{
-		if (side == 0)
+		if ((*format)[i] == '-')
 		{
-			while (width)
-			{
-				ft_putchar(*fill);
-				fill++;
-				width--;
-			}
+			neg = -1;
+			i++;
 		}
-		write(1, str, ft_strlen(str));
-		if (side == 1)
+		if ((*format)[i] == '*')
 		{
-			while (width)
-			{
-				ft_putchar(*fill);
-				fill++;
-				width--;
-			}
+			spec_infos->width = va_arg(var, int) * neg;
+			i++;
 		}
+		else
+			spec_infos->width = ft_atoi(&(*format)[i]) * neg;
+		while (((*format)[i] >= '0' && (*format)[i] <= '9') || (*format)[i] == '-')
+			i++;
 	}
-	else
+	if ((*format)[i] == '.')
 	{
-		ft_putstr(str);
+		i++;
+		if ((*format)[i] == '*')
+			spec_infos->precision = va_arg(var, int);
+		else
+			spec_infos->precision = ft_atoi(&(*format)[i]);		
+		while ((*format)[i] >= '0' && (*format)[i] <= '9')
+			i++;
 	}
+	return (spec_infos);
 }
-static void	shift_format(char **format)
-{
-	if (**format == '*')
-		(*format)++;
-	else if (**format >= '0' && **format <= '9')
-	{
-		while (**format >= '0' && **format <= '9')
-			(*format)++;
-	}
-}
-static char	*width_flag(char **format, va_list var, int *side, int *len)
-{
-	int nb;
-	char *str;
 
-	*side = 0;
-	nb = 0;
-	if (**format == '-')
+static char	*add_zero(char *str, int precision, int len)
+{
+	int i;
+	char *new;
+
+	precision = precision - len;
+	new = (char *)malloc(sizeof(char) * len + precision + 1);
+	i = 0;
+	while (precision != 0)
 	{
-		*side = 1;
-		(*format)++;
+		new[i] = '0';
+		i++;
+		precision--;
 	}
-	if (**format <= '9' && **format >= '0')
+	len = 0;
+	while (str[len] != '\0')
 	{
-		nb = nb + ft_atoi(*format);
-		*len = nb;
-		(*format)++;
+		new[i] = str[len];
+		i++;
+		len++;
 	}
-	else if (**format == '*')
+	new[i] = '\0';
+	return (new);
+}
+
+
+static int insert_spaces(char *str, int width)
+{
+	int i;
+
+	i = 0;
+
+	while (width != 0)
 	{
-		nb = va_arg(var, int);
-		*len = nb;
-		(*format)++;
+		str[i] = ' ';
+		width--;
+		i++;
 	}
-	if (nb < 1)
+	str[i] = '\0';
+	return (i);
+}
+static char *add_space(char *str, int width, int len)
+{
+	char *new;
+	int i;
+
+	if (width != 0)
 	{
-		*side = -1;
-		return (NULL);
-	}
-	str = (char *)malloc(sizeof(char) * nb + 1);
-	str[nb] = '\0';
-	while (nb != 0)
-	{
-		nb--;
-		str[nb] = ' ';
+
+		i = 0;
+		if (width > 0)
+		{	
+			width = width - len;
+			if (width < 0)
+				return (str);
+			new = (char *)malloc(sizeof(char) + len + width + 1);
+			i = insert_spaces(new, width);
+		}
+		else
+			new = (char *)malloc(sizeof(char) + len + (width * -1) + 1);
+		len = 0;
+		while (str[len] != '\0')
+		{
+			new[i] = str[len];
+			i++;
+			len++;
+		}
+		if (width < 0)
+		{	
+			width = (width * -1) - len;
+			return (str);
+			i = insert_spaces(new + i, width);
+		}
+		new[i] = '\0';
+		free(str);
+		return (new);
 	}
 	return (str);
-}
-static int precision_flag(char **format, va_list var)
-{
-	int nb;
-
-	nb = 0;
-	(*format)++;
-	if (**format <= '9' && **format >= '0')
-	{
-		nb = nb + ft_atoi(*format);
-		(*format)++;
-	}
-	else if (**format == '*')
-	{
-		nb = va_arg(var, int);
-		(*format)++;
-	}
-	return (nb);
-}
-static void	ft_putstr_with_precision(char *str, int precision)
-{
-	int len;
-
-	len = ft_strlen(str);
-	precision = precision - len;
-	if (precision < 0)
-		precision = precision * -1;
-	if (precision > 0)
-	{
-		printf("if | precision = %d\n", precision);
-		while (precision)
-		{
-			write(1, "0", 1);
-			precision--;
-		}
-		write(1, str, len);
-	}
-	else
-	{
-		printf("else | precision = %d\n", precision);
-
-		write(1, str, len);
-	}
-	/*
-	if (precision > 0)
-	{
-		if (precision < len)
-			write(1, str, precision);
-		else
-			write(1, str, len);
-	}
-	if (precision == -1)
-		write(1, str, len);
-		*/
 }
 
 void	integer_handler(char **format, va_list var)
 {
+	t_flag_attribs *spec_infos;
 	char *str;
-	int nb;
-	int	side;
 	int len = 0;
 
-	if (**format == 'd')
+	spec_infos = integer_parser(format, var);
+	printf("spec_infos->width = %d\n", spec_infos->width);
+	printf("spec_infos->precision = %d\n", spec_infos->precision);
+	str = ft_itoa(va_arg(var, int));
+	if (spec_infos->precision > 0)
 	{
-		str = ft_itoa(va_arg(var, int));
-		ft_putstr(str);
-		(*format)++;
-		return ;
+		len = ft_strlen(str);
+		str = add_zero(str, spec_infos->precision, len);
+	}
+	if (spec_infos->width != 0)
+	{
+		len = ft_strlen(str);
+		printf("len = %d\n", len);
+		str = add_space(str, spec_infos->width, len);
 	}
 	while (**format != 'd')
+		(*format)++;
+	(*format)++;
+	write(1, str, ft_strlen(str));
+	return ;
+}
+	else if (**format >= '0' && **format <= '9')
 	{
-		if (**format == '.')
-		{
-			nb = precision_flag(format, var);
-
-			shift_format(format);
-			ft_putstr_with_precision(ft_itoa(va_arg(var, int)), nb);
-			while (**format != 'd')
-				(*format)++;
+		while (**format >= '0' && **format <= '9')
 			(*format)++;
-			return ;
-		}
-		else if ((**format >= '0' && **format <= '9') || **format == '-' || **format == '*')
-		{
-			str = width_flag(format, var, &side, &len);
-			shift_format(format);
-			ft_putstr_with_width(str, ft_itoa(va_arg(var, int)), len, side);
-			while (**format != 'd')
-				(*format)++;
-			(*format)++;
-			return ;
-		}
 	}
 }
