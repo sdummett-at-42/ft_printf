1c7697acde50b267c96f2d5273571c28218fb574
Stone <stone.dummett@gmail.com>
2021-07-02 21:00:37 +0200

    Redone [%d] [%i] funcs

--- specifiers/d_i_specifier/integer_handler.c
+++ specifiers/d_i_specifier/integer_handler.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 02:24:06 by sdummett          #+#    #+#             */
-/*   Updated: 2021/07/02 14:31:25 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/07/02 20:58:31 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,108 +67,35 @@ static t_flag_attribs *integer_parser(char **format, va_list var)
 		i++;
 		if ((*format)[i] == '*')
 			spec_infos->precision = va_arg(var, int);
-		else
+		else if ((*format)[i] >= '0' && (*format)[i] <= '9')
 			spec_infos->precision = ft_atoi(&(*format)[i]);		
+		else
+			spec_infos->precision = -1;
 		while ((*format)[i] >= '0' && (*format)[i] <= '9')
 			i++;
 	}
 	return (spec_infos);
 }
 
-static char	*add_zero(char *str, int width_prec, int len, int flag)
+static char *pos_prec_handler(char *str, int precision, int len)
 {
 	int i;
-	int minus;
 	char *new;
 
-
-	if (str[0] == '-' && flag != 1)
-	{
-		if (width_prec < len)
-			return (str);
-	}
-	else if (width_prec <= len)
-	{
+	if (precision <= len)
 		return (str);
-	}
-	minus = -1;
-	width_prec = width_prec - len;
+	new = (char *)malloc(sizeof(char) * precision + 1);
+	if (new == NULL)
+		return (NULL);
 	i = 0;
-	if (str[i] == '-')
-	{
-		if (flag != 1)
-			width_prec = width_prec + 1;
-		new = (char *)malloc(sizeof(char) * len + width_prec + 1);
-		new[i] = '-';
-		str++;
-		i++;
-	}
-	else
-		new = (char *)malloc(sizeof(char) * len + width_prec + 1);
-	while (width_prec != 0)
+	precision = precision - len;
+	while (i < precision)
 	{
 		new[i] = '0';
 		i++;
-		width_prec--;
 	}
 	len = 0;
-	while (str[len] != '\0')
-	{
-		new[i] = str[len];
-		i++;
-		len++;
-	}
-	new[i] = '\0';
-	return (new);
-}
-
-static int insert_spaces(char *str, int width)
-{
-	int i;
-
-	i = 0;
-	while (width != 0)
-	{
-		str[i] = ' ';
-		width--;
-		i++;
-	}
-	return (i);
-}
-
-static char *add_space(char *str, int width, int len)
-{
-	char *new;
-	int i;
-
-	i = 0;
-	if (width < 0)
-	{
-		width = (width * -1) - len;
-		if (width < 0)
-			return (str);
-		new = (char *)malloc(sizeof(char) + len + \
-				width + 1);
-		len = 0;
-		while (str[len] != '\0')
-		{
-			new[i] = str[len];
-			i++;
-			len++;
-		}
-		insert_spaces(new + i, width);
-		i = i + width ;
-		new[i] = '\0';
-		free(str);
-		return (new);
-	}
-	width = width - len;
-	if (width < 0)
-		return (str);
-	new = (char *)malloc(sizeof(char) + len + width + 1);
-	i = insert_spaces(new, width);
-	len = 0;
-	while (str[len] != '\0')
+	while (str[len] != 0)
 	{
 		new[i] = str[len];
 		i++;
@@ -179,6 +106,144 @@ static char *add_space(char *str, int width, int len)
 	return (new);
 }
 
+static char *neg_prec_handler(char *str, int precision, int len)
+{
+	int i;
+	char *new;
+
+	if (!(precision + 1 >= len))
+		return (str);
+	new = malloc(sizeof(char) * precision + 2);
+	new[0] = '-';
+	precision = precision - (len - 1);
+	i = 1;
+	while (precision != 0)
+	{
+		new[i] = '0';
+		i++;
+		precision--;
+	}
+	precision = 1;
+	while (str[precision] != '\0')
+	{
+		new[i] = str[precision];
+		i++;
+		precision++;
+	}
+	new[i] = '\0';
+	free(str);
+	return (new);
+}
+
+static char *neg_wid_handler(char *str, int width, int len)
+{
+	int i;
+	char *new;
+
+	if (width <= len)
+		return (str);
+	new = malloc(sizeof(char) * width + 1);
+	if (new == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		new[i] = str[i];
+		i++;
+	}
+	while (i != width)
+	{
+		new[i] = ' ';
+		i++;
+	}
+	new[i] = '\0';
+	free(str);
+	return (new);
+}
+
+static char *pos_wid_handler(char *str, int width, int len)
+{
+	int i;
+	char *new;
+
+	if (width <= len)
+		return (str);
+	new = malloc(sizeof(char) * width + 1);
+	if (new == NULL)
+		return (NULL);
+	i = 0;
+	while (i < (width - len))
+	{
+		new[i] = ' ';
+		i++;
+	}
+	len = 0;
+	while (str[len] != '\0')
+	{
+		new[i] = str[len];
+		i++;
+		len++;
+	}
+	new[i]= '\0';
+	free(str);
+	return (new);
+}
+
+static char *precision_handler(char *str, int precision)
+{
+	int len;
+
+	len = ft_strlen(str);
+	if (str[0] == '-')
+		return (neg_prec_handler(str, precision, len));
+	else
+		return (pos_prec_handler(str, precision, len));
+}
+
+
+static char *width_handler(char *str, int width)
+{
+	int len;
+
+	len = ft_strlen(str);
+	if (width < 0)
+		return (neg_wid_handler(str, width * -1, len));
+	return (pos_wid_handler(str, width, len));
+}
+
+static char *zero_handler(char *str, int zero, int precision)
+{
+	int len;
+
+	len = ft_strlen(str);
+	if (precision == -1 || precision > 0)
+		return (width_handler(str, zero));
+	else
+		return (precision_handler(str, zero - 1));
+}
+
+void	integer_handler(char **format, va_list var, int *pft_ret)
+{
+	char *str;
+	(void)pft_ret;
+	t_flag_attribs *flag;
+
+	flag = integer_parser(format, var);
+	str = ft_itoa(va_arg(var, int));
+	if (flag->precision > 0)
+		str = precision_handler(str, flag->precision);
+	if (flag->width != 0)
+		str = width_handler(str, flag->width);
+	if (flag->zero > 0)
+		str = zero_handler(str, flag->zero, flag->precision);
+	write(1, str, ft_strlen(str));
+	while (**format != 'd' && **format != 'i')
+		(*format)++;
+	(*format)++;
+	free(str);
+}
+
+/*
 void	integer_handler(char **format, va_list var, int *ptf_ret)
 {
 	t_flag_attribs *spec_infos;
@@ -187,7 +252,7 @@ void	integer_handler(char **format, va_list var, int *ptf_ret)
 
 	spec_infos = integer_parser(format, var);
 	str = ft_itoa(va_arg(var, int));
-	if (spec_infos->precision > 0)
+	if (spec_infos->precision >= 0)
 	{
 		len = ft_strlen(str);
 		str = add_zero(str, spec_infos->precision, len, 0);
@@ -204,12 +269,10 @@ void	integer_handler(char **format, va_list var, int *ptf_ret)
 		len = ft_strlen(str);
 		str = add_space(str, spec_infos->width, len);
 	}
-	while (**format != 'd' && **format != 'i')
-		(*format)++;
-	(*format)++;
+
 	len = ft_strlen(str);
 	*ptf_ret = *ptf_ret + len;
 	write(1, str, len);
 	free(str);
 	free(spec_infos);
-}
+	}*/
