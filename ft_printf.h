73ea6ab5c43cbdec4262233d61e0e973732fa878
Stone <stone.dummett@gmail.com>
2021-07-08 01:17:45 +0200

    Norm

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/20 17:21:59 by sdummett          #+#    #+#             */
-/*   Updated: 2021/07/08 01:04:56 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/07/08 01:17:13 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,30 +32,31 @@ typedef struct s_attribs
 	int	negative;
 }		t_attribs;
 
-int	ft_printf(const char *fmt, ...)
-	__attribute__((format (printf, 1, 2)));
-int		check_specifier(char **fmt, va_list var);
-int		search_specifier(char **fmt);
-void	call_handler(int handler_choice, char **fmt, va_list var, int *ptf_ret);
-void	percent_handler(char **fmt, va_list var, int *ptf_ret);
-void	char_handler(char **fmt, va_list var, int *ptf_ret);
-void	str_handler(char **fmt, va_list var, int *ptf_ret);
-void	pointer_handler(char **fmt, va_list var, int *ptf_ret);
-void	integer_handler(char **fmt, va_list var, int *ptf_ret);
-void	u_integer_handler(char **fmt, va_list var, int *ptf_ret);
-void	hexalow_handler(char **fmt, va_list var, int *ptf_ret);
-void	hexaup_handler(char **fmt, va_list var, int *ptf_ret);
-char	*ft_uitoa(unsigned int n);
-void	count_and_display(char **fmt, char *str, char c, int *ptf_ret);
-char	*uitohex_low(unsigned long nb);
-char	*uitohex_up(unsigned long nb);
-char	*check_if_eq_zero(char *str, int dot);
-char	*precision_handler(char *str, int precision);
-char	*width_handler(char *str, int width);
-char	*padding_handler(char *str, int padding, int precision, int dot);
-void	integer_handler(char **fmt, va_list var, int *ptf_ret);
+int			ft_printf(const char *fmt, ...)
+			__attribute__((format (printf, 1, 2)));
+int			check_specifier(char **fmt, va_list var);
+int			search_specifier(char **fmt);
+void		call_handler(int handler_choice, char **fmt, va_list var, \
+		int *ptf_ret);
+void		percent_handler(char **fmt, va_list var, int *ptf_ret);
+void		char_handler(char **fmt, va_list var, int *ptf_ret);
+void		str_handler(char **fmt, va_list var, int *ptf_ret);
+void		pointer_handler(char **fmt, va_list var, int *ptf_ret);
+void		integer_handler(char **fmt, va_list var, int *ptf_ret);
+void		u_integer_handler(char **fmt, va_list var, int *ptf_ret);
+void		hexalow_handler(char **fmt, va_list var, int *ptf_ret);
+void		hexaup_handler(char **fmt, va_list var, int *ptf_ret);
+char		*ft_uitoa(unsigned int n);
+void		count_and_display(char **fmt, char *str, char c, int *ptf_ret);
+char		*uitohex_low(unsigned long nb);
+char		*uitohex_up(unsigned long nb);
+char		*check_if_eq_zero(char *str, int dot);
+char		*precision_handler(char *str, int precision);
+char		*width_handler(char *str, int width);
+char		*padding_handler(char *str, int padding, int precision, int dot);
+void		integer_handler(char **fmt, va_list var, int *ptf_ret);
 t_attribs	*struct_init(void);
 t_attribs	*fmt_parser(char **fmt, va_list var);
-char	*prefix_handler(char *str, char spec);
+char		*prefix_handler(char *str, char spec);
 
 #endif
